dcbd48b2cfeefa0eb18308baa881dfb365c94cf9
Austin Baccus <austin.baccus@wsu.edu>
2020-06-14 17:07:19 -0700

    Simplified the loop function, and filtered the results to only read speed and RPM data.

--- AutoTelemetry/Arduino/custom/custom.ino
+++ AutoTelemetry/Arduino/custom/custom.ino
@@ -16,48 +16,66 @@ unsigned char len = 0;
 unsigned char buf[8];
 unsigned int canID;
 
+unsigned char car_speed;
+unsigned int engine_rpm;
+unsigned char temp;
+
 void setup()
 {
-  Serial.begin(115200);   //to communicate with Serial monitor
-  
-//tries to initialize, if failed --> it will loop here for ever
+  Serial.begin(115200); // This is for communicating with the serial monitor
+
+//tries to initialize, if failed --> it will loop here forever
 START_INIT:
 
     if(CAN_OK == CAN.begin(CAN_500KBPS))      //setting CAN baud rate to 500Kbps
     {
-        Serial.println("CAN BUS Shield init ok!");
+        Serial.println("Init success!");
     }
     else
     {
-        Serial.println("CAN BUS Shield init fail");
-        Serial.println("Init CAN BUS Shield again");
-        delay(100);
+        Serial.println("Init failed");
+        delay(1000);
         goto START_INIT;
     }
 }
 
 
-
 void loop()
 {
-    if(CAN_MSGAVAIL == CAN.checkReceive())    //check if data is coming
+    if(CAN_MSGAVAIL == CAN.checkReceive())        //check if data coming
     {
         CAN.readMsgBuf(&len, buf);    //read data,  len: data length, buf: data buffer
         canID = CAN.getCanId();       //getting the ID of the incoming message
 
-        if (canID == 0x0C)            //it will only read messages with ID = 0x0C (used to be 0xF2)
+        if (canID == 0xF1)            //reading only our beloved 0xF1 message
         {
           Serial.print("ID is: ");
           Serial.print(canID, HEX);     //printing the ID in its standard form, HEX
 
           Serial.print("    Length is: ");
           Serial.println(len);
-        
-          for(int i = 0; i<len; i++)    //looping on the incoming data to print them
-          {
-              Serial.write(buf[i]);     //Serial.write prints the character itself
-          }
-          Serial.println("\n\t*****************\n");
+
+          Serial.println("\n\n");
+
+          //for the first signal: car speed
+          car_speed = buf[0];
+          
+          Serial.print("Car speed = ");
+          Serial.println(car_speed);
+
+
+
+          //for the second signal: engine RPM
+          engine_rpm = buf[1];
+          engine_rpm = engine_rpm << 8;
+          temp = buf[2] & 0x3F;
+          engine_rpm = engine_rpm + temp;
+
+          Serial.print("Engine RPM = ");
+          Serial.println(engine_rpm);
+
+          
+          Serial.println("\n\n\t*************");
         }
     }
 }
