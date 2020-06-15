97edd33114c2204e502493481b2e1c4871ee7065
Austin Baccus <austin.baccus@wsu.edu>
2020-06-14 18:27:23 -0700

    Added code for sending PID request for RPM.

--- AutoTelemetry/Arduino/custom/custom.ino
+++ AutoTelemetry/Arduino/custom/custom.ino
@@ -1,23 +1,13 @@
-/**************************
- *         Author         *
- *      omarCartera       *
- *                        *
- *        5/8/2017        *
- *                        *
- * c.omargamal@gmail.com  *
- **************************/
-
 #include <SPI.h>          //SPI is used to talk to the CAN Controller
 #include <mcp_can.h>
 
-MCP_CAN CAN(10);          //set SPI Chip Select to pin 10
+#define CAN_ID_PID 0x7DF
+
+MCP_CAN CAN(9);          //set SPI Chip Select to pin 9
 
 unsigned char len = 0;
 unsigned char buf[8];
 unsigned int canID;
-
-unsigned char car_speed;
-unsigned int engine_rpm;
 unsigned char temp;
 
 void setup()
@@ -42,40 +32,39 @@ START_INIT:
 
 void loop()
 {
-    if(CAN_MSGAVAIL == CAN.checkReceive())        //check if data coming
+  sendPid();
+  readResponse();
+}
+
+void readResponse()
+{
+  if(CAN_MSGAVAIL == CAN.checkReceive()) //check if data coming
+  {
+    CAN.readMsgBuf(&len, buf);    //read data,  len: data length, buf: data buffer
+    canID = CAN.getCanId();       //getting the ID of the incoming message
+
+    // 41 0C 0F A0
+    // 0x0FA0 = 4000
+    // 4000 / 4 = 1000 rpm
+    
+    if (canID == 0x7DF)           //reading only our beloved 0xF1 message
     {
-        CAN.readMsgBuf(&len, buf);    //read data,  len: data length, buf: data buffer
-        canID = CAN.getCanId();       //getting the ID of the incoming message
-
-        if (canID == 0xF1)            //reading only our beloved 0xF1 message
-        {
-          Serial.print("ID is: ");
-          Serial.print(canID, HEX);     //printing the ID in its standard form, HEX
-
-          Serial.print("    Length is: ");
-          Serial.println(len);
-
-          Serial.println("\n\n");
-
-          //for the first signal: car speed
-          car_speed = buf[0];
-          
-          Serial.print("Car speed = ");
-          Serial.println(car_speed);
-
-
-
-          //for the second signal: engine RPM
-          engine_rpm = buf[1];
-          engine_rpm = engine_rpm << 8;
-          temp = buf[2] & 0x3F;
-          engine_rpm = engine_rpm + temp;
-
-          Serial.print("Engine RPM = ");
-          Serial.println(engine_rpm);
-
-          
-          Serial.println("\n\n\t*************");
-        }
+      Serial.print(canID);
+      Serial.print("\t");
+      for (int i = 0; i < len; i++)
+      {
+        Serial.print(buf[i]);
+        Serial.print("\t");
+      }
+      Serial.println();
     }
+  }
+}
+
+void sendPid() 
+{
+  unsigned char tmp[8] = {0x02, 0x01, 0x0C, 0, 0, 0, 0, 0};
+  Serial.print("SEND PID: 0x");
+  Serial.println(0x0C, HEX);
+  CAN.sendMsgBuf(CAN_ID_PID, 0, 8, tmp);
 }
