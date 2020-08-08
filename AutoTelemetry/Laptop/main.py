d2453f6dcbeb69d49d0576826d2e0bf2b3a6846e
Poonslayer69420 <austinbaccus@live.com>
2020-08-07 19:08:44 -0700

    New C# script

--- AutoTelemetry/Laptop/main.py
+++ AutoTelemetry/Laptop/main.py
@@ -2,20 +2,32 @@ import serial
 import time
 import csv
 
-ser = serial.Serial('COM5')
+ser = serial.Serial(port = "COM7", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 ser.flushInput()
+
+while True:
+    try:
+        ser_bytes = ser.readline()
+
+        rpm = ser_bytes.decode("utf-8")
+        print(rpm)
+    except:
+        print("Keyboard Interrupt")
+        break
 
 with open("rpm_data.csv","a",newline='') as f:
     writer = csv.writer(f,delimiter=",")
+    
     while True:
         try:
             ser_bytes = ser.readline()
 
-            rpm = int(ser_bytes.decode("utf-8"))
+            rpm = ser_bytes.decode("utf-8")
             print(rpm)
 
             localtime = time.asctime(time.localtime(time.time())).split()
             writer.writerow([localtime[0],localtime[1],localtime[2],localtime[3],localtime[4],rpm])
+            time.sleep(0.1)
         except:
             print("Keyboard Interrupt")
             break
---
+++ AutoTelemetry/LaptopCs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.IO;
+
+namespace LaptopCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            SerialPort _serialPort = new SerialPort("COM7", 115200);
+            _serialPort.Open();
+
+            var csv = new StringBuilder();
+
+            Console.CancelKeyPress += delegate
+            {
+                File.AppendAllText(@"C:\Users\gunra\source\repos\AutoTelemetry\AutoTelemetry\Laptop\stephen_car_data_2.csv", csv.ToString());
+            };
+
+            while (true)
+            {
+                string s = _serialPort.ReadLine();
+                csv.Append(s);
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
