74f6d2373e429ed6189ad46b8ac0d56508a5d0a2
Austin Baccus <austin.baccus@wsu.edu>
2020-08-07 20:29:12 -0700

    Split code from Main() into smaller functions. Addeda  10 second timeout for retrieving CAN messages. Program now awaits user confirmation before collecting data. Timestamp is added into each message.

--- AutoTelemetry/LaptopCs/Program.cs
+++ AutoTelemetry/LaptopCs/Program.cs
@@ -2,32 +2,109 @@
 using System.IO.Ports;
 using System.Text;
 using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace LaptopCs
 {
     class Program
     {
+        static SerialPort _serialPort;
+        static StringBuilder csv;
+        static StringBuilder msg;
+        static Stopwatch sw;
+        static TimeSpan ts;
+
         static void Main(string[] args)
         {
+            // for whatever reason the console prints out black text on a black background by default, so we have to specify the colors
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
+            
+            // flag for whether data has been saved to a file or not
+            bool isDataSaved = false;
 
-            SerialPort _serialPort = new SerialPort("COM7", 115200);
+            // open serial connection
+            _serialPort = new SerialPort("COM7", 115200);
             _serialPort.Open();
 
-            var csv = new StringBuilder();
+            // initialize the new StringBuilder objects for better string concat performance
+            csv = new StringBuilder();
+            msg = new StringBuilder();
 
+            // initialize the stopwatches
+            sw = new Stopwatch();
+
+            // stop logging delegate
             Console.CancelKeyPress += delegate
             {
-                File.AppendAllText(@"C:\Users\gunra\source\repos\AutoTelemetry\AutoTelemetry\Laptop\stephen_car_data_2.csv", csv.ToString());
+                // the user wants to exit the program. this saves the data before exiting
+                SaveTelemetryToFile();
+                isDataSaved = true;
             };
 
+            // wait for user input to start recording data
+            Console.Write("Press 'enter' to start logging data. Press Ctrl-C to stop logging data. ");
+            Console.ReadLine();
+
+            // start the logging loop
+            Loop();
+
+            // save the data before exiting the program
+            if (isDataSaved == false) 
+                SaveTelemetryToFile();
+        }
+        static void Loop()
+        {
+            // start the stopwatch
+            sw.Start();
+
+            string canMessage;
+
+            // enter the main loop
             while (true)
             {
-                string s = _serialPort.ReadLine();
-                csv.Append(s);
-                Console.WriteLine(s);
+                // get the elapsed time
+                ts = sw.Elapsed;
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+                // read in the data from the CAN
+                var task = Task.Run(() => GetCANMessage());
+
+                if (task.Wait(TimeSpan.FromSeconds(10)))
+                {
+                    canMessage = task.Result;
+
+                    // join the timestamp and the CAN message together
+                    msg.AppendJoin(',', new string[] { elapsedTime, canMessage });
+
+                    // append CAN message to the csv file buffer text
+                    csv.Append(msg.ToString());
+
+                    // print CAN message to the console
+                    Console.WriteLine(msg.ToString());
+                }
+                else
+                {
+                    // it's been a while since we got the last message, the car is probably turned off at this point
+                    // in this case, we can exit the loop and save the data to the file
+                    break;
+                }
             }
+
+            // stop the stopwatch
+            sw.Stop();
+        }
+        static string GetCANMessage()
+        {
+            // read in the latest serial input from the micro-controller
+            return _serialPort.ReadLine();
+        }
+        static void SaveTelemetryToFile()
+        {
+            string datetime = DateTime.Today.ToString("s");
+            string path = @"C:\Users\gunra\source\repos\AutoTelemetry\AutoTelemetry\Laptop\" + datetime + ".csv";
+            File.AppendAllText(path, csv.ToString());
         }
     }
 }
